28ef811103e0519d7ca7e9e0a58012db54adaf51
Cyan <wzwkak@gmail.com>
2015-11-28 12:47:14 +0800

    Add school model
    Create schools model and
    
    Set up has_many connection with Users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126055155) do
+ActiveRecord::Schema.define(version: 20151127144613) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "msg",        limit: 255
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20151126055155) do
   add_index "punches", ["average_time"], name: "index_punches_on_average_time", using: :btree
   add_index "punches", ["punchable_type", "punchable_id"], name: "punchable_index", using: :btree
 
+  create_table "schools", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",               limit: 255, default: "", null: false
     t.string   "encrypted_password",  limit: 255, default: "", null: false
@@ -59,6 +65,7 @@ ActiveRecord::Schema.define(version: 20151126055155) do
     t.string   "last_sign_in_ip",     limit: 255
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
+    t.integer  "school_id",           limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
