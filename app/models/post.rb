ac2c797648202efd40d1e36b414e3daffeb7fcc4
CarlosLin <weihung0418@gmail.com>
2015-12-10 14:44:04 +0800

    Create post type

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   has_many :favorites, :as => :favoritable
   has_many :messages, :inverse_of => :post
   belongs_to :user
+  belongs_to :type, :inverse_of => :posts
   has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
