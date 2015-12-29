c915321513b6013daba84140ae0c2ca8b7481e30
CarlosLin <weihung0418@gmail.com>
2015-12-29 18:45:30 +0800

    fix cover img with parse ptt

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,9 +1,18 @@
 module PostHelper
+	 # ***************
+	 #     首頁圖片
+	 # ***************
   def index_cover_image(post)
     if post.avatar_file_name?
       link_to (image_tag post.avatar.url(:medium)), post,class: 'thumbnail'
     else
       link_to (image_tag post.cover_image), post, class: 'thumbnail'
     end
+  end
+  # ***************
+	#  	 show_page
+  # ***************
+  def have_avatar_file
+  	image_tag @post.avatar.url(:medium) if @post.avatar_file_name?
   end
 end
