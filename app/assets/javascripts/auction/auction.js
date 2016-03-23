e937488af1b7ab4fe6791a0ef0368d214ac556f6
Cyan <wzwkak@gmail.com>
2016-03-23 15:59:05 +0800

    move javascript to the right place

--- app/assets/javascripts/auction/auction.js
+++ app/assets/javascripts/auction/auction.js
@@ -3,3 +3,13 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require bootsy
+function sub_cate(value){
+$.ajax({
+  type: "GET",
+  url: '/sub_cate?id='+value,
+  data: 'text',
+  success: function(data){
+    $(".auction").html(data);
+  }
+})
+}
